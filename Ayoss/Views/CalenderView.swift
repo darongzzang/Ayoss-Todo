8e5badd9a64efb245de0c2d2ed0bb46d89beb8e7
JaeSunEo <wotjs020708@gmail.com>
2025-05-24 19:59:43 +0900

    CalenderView 파일 생성 및 headerView 추가

---
+++ Ayoss/Views/CalenderView.swift
@@ -0,0 +1,18 @@
+//
+//  CalenderView.swift
+//  Ayoss
+//
+//  Created by 어재선 on 5/24/25.
+//
+
+import SwiftUI
+
+struct CalenderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalenderView()
+}
